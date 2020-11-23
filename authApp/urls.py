476c8e0e9b7706f723172bd6f49f276c5f9b65a2
cbrianbet <cbrianbet@outlook.com>
2020-11-23 16:22:44 +0300

    added partner user lrole and permissions

--- authApp/urls.py
+++ authApp/urls.py
@@ -20,6 +20,7 @@ urlpatterns = [
     path('web/facility-partner-link/', views.facility_partner_link, name='facility-partner-link'),
     path('web/designation-list/', views.designation_list, name='designation-list'),
     path('web/register-partner/', views.register_partner, name='register-partner'),
+    path('web/register-partner-user/', views.register_partner_user, name='register-partner-user'),
     path('web/register-fac-admin/', views.register_fac_admin, name='register-fac-admin'),
     path('web/list-fac-admin/', views.facility_admin_list, name='list-fac-admin'),
     path('web/edit-partner/<int:p_id>/', views.edit_partner, name='edit-partner'),
