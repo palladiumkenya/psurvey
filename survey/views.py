a16ef92fe4df95c5c7312a335e424d327e1532df
CynthiaKamau <cynthiakamau54@gmail.com>
2021-05-27 10:31:52 +0000

    Lost Server static

--- survey/views.py
+++ survey/views.py
@@ -83,7 +83,7 @@ def get_consent(request):
                                                    firstname=request.data['first_name'])
     session.save()
     return JsonResponse({
-        'link': 'http://127.0.0.1:8000/api/questions/answer/{}'.format(a_id),
+        'link': 'https://psurvey-api.mhealthkenya.co.ke/api/questions/answer/{}'.format(a_id),
         'session': session.pk
     })
     # return Res({"Question": serializer.data, "Ans": ser.data, "session_id": session.pk}, status.HTTP_200_OK)
@@ -162,7 +162,7 @@ def answer_question(request):
                 if index < (l - 1):
                     next_ = questions[index + 1]
                     return JsonResponse({
-                        'link': 'http://127.0.0.1:8000/api/questions/answer/{}'.format(next_.id),
+                        'link': 'https://psurvey-api.mhealthkenya.co.ke/api/questions/answer/{}'.format(next_.id),
                         "session_id": serializer.data['session']
                     })
 
@@ -208,7 +208,7 @@ def check_answer_algo(ser):
                 queryset = Answer.objects.filter(question=next_)
                 ans_ser = AnswerSerializer(queryset, many=True)
                 return JsonResponse({
-                    'link': 'http://127.0.0.1:8000/api/questions/answer/{}'.format(next_.id),
+                    'link': 'https://psurvey-api.mhealthkenya.co.ke/api/questions/answer/{}'.format(next_.id),
                     "session_id": ser.data['session']
                 })
 
