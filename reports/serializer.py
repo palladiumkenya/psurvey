b92f11d6851f77d8f5601aecf4aab4ef1e9a744e
cbrianbet <cbrianbet@outlook.com>
2022-04-08 23:20:32 +0300

    questionnaire summary

--- reports/serializer.py
+++ reports/serializer.py
@@ -28,6 +28,24 @@ class RespSerializer(serializers.ModelSerializer):
         return data
 
 
+class QuestionnaireRespSerializer(serializers.ModelSerializer):
+    answer = AnswerSerializer(read_only=True)
+
+    class Meta:
+        model = Response
+        fields = '__all__'
+
+    def to_representation(self, value):
+        data = super().to_representation(value)
+        ques = Question.objects.get(id=data['question'])
+        quesnn = Questionnaire.objects.get(id=ques.questionnaire.id)
+        data.update({
+            'question': QuestionSerializer(ques).data,
+            'questionnaire': QuestionnaireSerializer(quesnn).data})
+
+        return data
+
+
 class AllUserSerializer(serializers.ModelSerializer):
     designation = DesignationSerializer(read_only=True)
     facility = FacilitySerializer(read_only=True)
